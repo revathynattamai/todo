b58947d639a1cea276ff5bc90c114510c1505d11
revathynattamai <revathy.ng@gmail.com>
2017-11-13 22:43:12 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+const todo = ["Ani", "Aaru"];
 
 class Hello extends Component {
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleChange(e){
+        console.log(e.target.value);
+        this.props.onintext(e.target.value);
+    }
+
+    handleAdd(){
+        console.log("todo");
+    }
+
     render() {
-        return (
+        return ( 
             <div>
-                Hello World
+                Enter input <input type="text"
+                             value={this.props.intext} 
+                             onChange={this.handleChange} />
+                <button onClick={this.handleAdd}>Add</button>
+                
             </div>
         );
     }
 }
 
+class Main extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            intext:""
+        }
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    }
+
+    handleFilterTextChange(intext){
+        console.log(intext);
+        this.setState({
+            intext: intext
+        });
+    }
+
+    render() {
+            return (
+                <div>
+                    <Hello 
+                    intext={this.state.intext}
+                    onintext={this.handleFilterTextChange}/>
+                    {this.props.list}
+                </div>
+            );
+
+
+    }
+}
+
 ReactDOM.render(
-    <Hello />,
+    <Main list={todo} />,
     document.getElementById('root')
-);
+); 
