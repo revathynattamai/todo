ddcd8b3fa43e8eb4a5187f6c303477c4e53f3ffd
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-11-14 10:00:16 +0000

    displays todo list

--- src/app.js
+++ src/app.js
@@ -2,62 +2,61 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class Hello extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            todo: ["Ani", "Aaru"],
-             input: ""}
-        this.handleChange = this.handleChange.bind(this);
-        this.handleAdd = this.handleAdd.bind(this);
-        this.handleRemove = this.handleRemove.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: ["Ani", "Aaru"],
+      input: ""
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+  }
 
-    handleChange(e) {
-        this.setState({ input: e.target.value })
+  handleChange(e) {
+  this.setState({ input: e.target.value })
+  }
+
+  handleAdd() {
+    let a = this.state.input;
+    if (a.length == 0) {
+      alert("Please enter a value");
+    } else {
+      // this.state.todo.push(this.state.input);
+      let newTodo = this.state.todo;
+      newTodo.push(a);
+      this.setState({
+        todo: newTodo
+      });
     }
+  }
 
-    handleAdd() {
-        let a = this.state.input;
-        if (a.length==0){
-            alert("Please enter a value");
-        } else {
-            this.state.todo.push(this.state.input);
-        }
-        console.log(this.state.todo);
+  handleRemove() {
+    let b = this.state.input;
+    let c = this.state.todo;
+    let index = c.indexOf(b);
+    if (index > -1) {
+      c.splice(index, 1);
     }
-
-    handleRemove() {
-        let b = this.state.input;
-        let c = this.state.todo;
-        let index = c.indexOf(b);
-        if(index > -1){
-            c.splice(index, 1);
-        }
-        else{
-            alert("Element not found");
-        }
-        console.log(c);
+    else {
+      alert("Element not found");
     }
+    console.log(c);
+  }
 
-    render() {
-
-        const displayTodos = () => {
-            console.log(this.state.todo.forEach(todo => <span>{todo}</span>));
-        }
-        return (
-            <div>
-                Enter input <input type="text"
-                    onChange={this.handleChange} />
-                <button onClick={this.handleAdd}>Add</button>
-                <button onClick={this.handleRemove}>Remove</button>
-
-                    {displayTodos()}
-            </div>
-        );
-    }
+  render() {
+  return (
+      <div>
+        Enter input <input type="text" onChange={this.handleChange} />
+        <button onClick={this.handleAdd}>Add</button>
+        <button onClick={this.handleRemove}>Remove</button>
+        {this.state.todo.map(todo => <li>{todo}</li>)}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-    <Hello />,
-    document.getElementById('root')
-); 
+  <Hello />,
+  document.getElementById('root')
+);
