f1c2dccafe06c9ffd6fc1fc82c409dbdb6d96bbf
Revathy <revathy.gopinath@wipro.com>
2017-11-14 14:56:49 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,67 +1,63 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-const todo = ["Ani", "Aaru"];
 
 class Hello extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
+        this.state = { 
+            todo: ["Ani", "Aaru"],
+             input: ""}
         this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
     }
 
-    handleChange(e){
-        console.log(e.target.value);
-        this.props.onintext(e.target.value);
+    handleChange(e) {
+        this.setState({ input: e.target.value })
     }
 
-    handleAdd(){
-        console.log("todo");
+    handleAdd() {
+        let a = this.state.input;
+        if (a.length==0){
+            alert("Please enter a value");
+        } else {
+            this.state.todo.push(this.state.input);
+        }
+        console.log(this.state.todo);
+    }
+
+    handleRemove() {
+        let b = this.state.input;
+        let c = this.state.todo;
+        let index = c.indexOf(b);
+        if(index > -1){
+            c.splice(index, 1);
+        }
+        else{
+            alert("Element not found");
+        }
+        console.log(c);
     }
 
     render() {
-        return ( 
+
+        const displayTodos = () => {
+            console.log(this.state.todo.forEach(todo => <span>{todo}</span>));
+        }
+        return (
             <div>
                 Enter input <input type="text"
-                             value={this.props.intext} 
-                             onChange={this.handleChange} />
+                    onChange={this.handleChange} />
                 <button onClick={this.handleAdd}>Add</button>
-                
+                <button onClick={this.handleRemove}>Remove</button>
+
+                    {displayTodos()}
             </div>
         );
     }
 }
 
-class Main extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            intext:""
-        }
-        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-    }
-
-    handleFilterTextChange(intext){
-        console.log(intext);
-        this.setState({
-            intext: intext
-        });
-    }
-
-    render() {
-            return (
-                <div>
-                    <Hello 
-                    intext={this.state.intext}
-                    onintext={this.handleFilterTextChange}/>
-                    {this.props.list}
-                </div>
-            );
-
-
-    }
-}
-
 ReactDOM.render(
-    <Main list={todo} />,
+    <Hello />,
     document.getElementById('root')
 ); 
