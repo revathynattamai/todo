f3f12f4d66aeb3ff2d246f1ddfbfb23cf90ae37b
Revathy <revathy.gopinath@wipro.com>
2017-11-14 19:41:52 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+// function DisplayTodos(props){
+//         console.log(props);
+//         return (
+//         <div>{props.forEach(todo => <span>{todo}</span>)}</div>);
+//     }
+
+class DisplayTodos extends Component {
+    render() {
+        let a = this.props.inp;
+        return(
+            <div>
+                {/* {a.forEach(a=>(<div>{a}</div>))} */}
+                a.map=(item,i)=>{
+                    
+                }
+                </div>
+                );
+    }
+}
+
+
 class Hello extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             todo: ["Ani", "Aaru"],
-             input: ""}
+            input: ""
+        }
         this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
         this.handleRemove = this.handleRemove.bind(this);
@@ -18,44 +40,39 @@ class Hello extends Component {
 
     handleAdd() {
         let a = this.state.input;
-        if (a.length==0){
+        let c = this.state.todo;
+        if (a.length == 0) {
             alert("Please enter a value");
         } else {
-            this.state.todo.push(this.state.input);
+            c.push(this.state.input);
         }
-        console.log(this.state.todo);
     }
 
     handleRemove() {
         let b = this.state.input;
         let c = this.state.todo;
         let index = c.indexOf(b);
-        if(index > -1){
+        if (index > -1) {
             c.splice(index, 1);
         }
-        else{
+        else {
             alert("Element not found");
         }
-        console.log(c);
     }
 
     render() {
-
-        const displayTodos = () => {
-            console.log(this.state.todo.forEach(todo => <span>{todo}</span>));
-        }
         return (
             <div>
                 Enter input <input type="text"
                     onChange={this.handleChange} />
                 <button onClick={this.handleAdd}>Add</button>
                 <button onClick={this.handleRemove}>Remove</button>
-
-                    {displayTodos()}
+                <DisplayTodos inp={this.state.todo} />
             </div>
         );
     }
 }
+
 
 ReactDOM.render(
     <Hello />,
